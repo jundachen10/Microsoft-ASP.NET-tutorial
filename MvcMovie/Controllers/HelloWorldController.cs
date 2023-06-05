70b27595ee88de1f053c1ac504763e0d49ccd6f5
jundachen10 <jundachen10@gmail.com>
2023-06-05 11:39:01 -0400

    second commit

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace MvcMovie.Controllers
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
 {
-    public class HelloWorldController : Controller
+    // GET: /HelloWorld/
+    public string Index()
     {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return "This is my default action...";
+    }
+    // GET: /HelloWorld/Welcome/
+    public string Welcome()
+    {
+        return "This is the Welcome action method";
     }
 }
-
